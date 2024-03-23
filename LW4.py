71cf7aa0b72197660298a8673b1fee987cb496b2
IvanMA4HEB <ifetchenko2017@gmail.com>
2024-03-23 11:53:48 +1000

    13123

--- LW4.py
+++ LW4.py
@@ -1,28 +1,50 @@
 import random
 userName = str(input('Введите ваше имя:'))
-if len(userName) > 360:
+if len(userName) > 600:
     print ("Вы превысили число допустимых символов!")
 else:
     print ("Вы зарегистрированы")
 userEmail = str(input("Введите вашу почту:"))
-if len(userEmail) > 600:
+if len(userEmail) > 360:
     print ("Вы превысили число допустимых символов!")
 else:
     print ("Вы зарегистрированы")
-userId = sum(random() <= 1 > 99999999)
-userPnum = sum(random() > 0 < 20)
+userId = random.randint(1, 99999999)
+print (userId)
+userPnum = int(input("7"))
+if (userPnum) < 999999999999:
+    print("+7", (userPnum))
+else:
+    print('Error')
 
 
 
 manageName = str(input('Введите ваше имя:'))
-if len(manageName) > 360:
+if len(manageName) > 600:
     print ("Вы превысили число допустимых символов!")
 else:
     print ("Вы зарегистрированы")
 manageEmail = str(input("Введите вашу почту:"))
-if len(manageEmail) > 600:
+if len(manageEmail) > 360:
     print ("Вы превысили число допустимых символов!")
 else:
     print ("Вы зарегистрированы")
-manageId = sum(random() <= 1 > 99999999)
-managePnum = sum(random() > 0 < 20)
+manageId = random.randint(1, 99999999)
+print (manageId)
+managePnum = int(input("7"))
+if (managePnum) < 999999999999:
+    print("+7", (managePnum))
+else:
+    print('Error')
+
+zakazNumbers = random.randint(1, 99999999)
+zakazId = random.randint(1, 99999999)
+zakazSum = int(input('Введите кол-во товара:'))
+if (((zakazSum) < 1000000) & ((zakazSum) > 0)):
+    print("Заказ оформлен")
+else:
+    
+    print('Недопустимое количество')
+
+
+
