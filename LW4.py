aa4d8e5bd79fb531fb492e456ec1b8a9d26ca566
IvanMA4HEB <ifetchenko2017@gmail.com>
2024-03-26 13:23:07 +1000

    update2

--- LW4.py
+++ LW4.py
@@ -1,59 +1,97 @@
 import random
 # Данные пользователя
-userName = str(input('Введите ваше имя:'))
-if len(userName) > 600:
-    print ("Вы превысили число допустимых символов!")
-else:
-    print ("Вы зарегистрированы")
-userEmail = str(input("Введите вашу почту:"))
-if len(userEmail) > 360:
-    print ("Вы превысили число допустимых символов!")
-else:
-    print ("Вы зарегистрированы")
-userId = random.randint(1, 99999999)
-print (userId)
-userPnum = int(input("7"))
-if (userPnum) < 999999999999:
-    print("+7", (userPnum))
-else:
-    print('Error')
-
-
-
-manageName = str(input('Введите ваше имя:'))
-if len(manageName) > 600:
-    print ("Вы превысили число допустимых символов!")
-else:
-    print ("Вы зарегистрированы")
-manageEmail = str(input("Введите вашу почту:"))
-if len(manageEmail) > 360:
-    print ("Вы превысили число допустимых символов!")
-else:
-    print ("Вы зарегистрированы")
-manageId = random.randint(1, 99999999)
-print (manageId)
-managePnum = int(input("7"))
-if (managePnum) < 999999999999:
-    print("+7", (managePnum))
-else:
-    print('Error')
-
-zakazNumbers = random.randint(1, 99999999)
-zakazId = random.randint(1, 99999999)
-zakazSum = int(input('Введите кол-во товара:'))
-if (((zakazSum) < 1000000) & ((zakazSum) > 0)):
-    print("Заказ оформлен")
-else:
+class user:
+    Name = ""
+    userId = random.randint(1, 99999999)
+    Email  =  str (input("введите вашу почту:"))
+    userPnum = int
     
-    print('Недопустимое количество')
+    def init(self):
+        
+        self.Name = str (input('Введите ваше имя:'))
+        # check_name_length(self)
+        self.userId = random.randint(1, 99999999)
+        self.Email = str (input("Введите вашу почту:"))
+        if len(self.Email) > 360:
+            print ("Вы превысили число допустимых символов!")
+        else:
+            print ("Вы зарегистрированы")
+        self.userPnum = int(input("7"))
+        if (self.userPnum) < 999999999999:
+            print("+7", (self.userPnum))
+        else:
+            print('Error')
+        
+    def check_name_length(self):
+        if len (self.Name) > 600:
+            print ("Вы превысили число допустимых символов!")
+        else:
+            print ("Вы зарегистрированы")
+            #self - ключевая команда ссылающая экзампляр класса(Элемент класса) 
+
+class manager:
+    manageName = ""
+    manageEmail = str(input('Введите ваше имя:'))
+    manageId = random.randint(1, 99999999)
+    managePnum =  int(input("7"))
+    
+    def unit(self):
+        # Данные менеджера
+          self.manageName = str(input('Введите ваше имя:'))
+        if len(self.manageName) > 600:
+            print ("Вы превысили число допустимых символов!")
+        else:
+            print ("Вы зарегистрированы")
+        self.manageEmail = str(input("Введите вашу почту:"))
+        if len(self.manageEmail) > 360:
+            print ("Вы превысили число допустимых символов!")
+        else:
+            print ("Вы зарегистрированы")
+        self.manageId = random.randint(1, 99999999)
+        print (self.manageId)
+        self.managePnum = int(input("7"))
+        if (self.managePnum) < 999999999999:
+            print("+7", (self.managePnum))
+        else:
+            print('Error')
+  
+  
+class Zakaz:
+    zakazNumbers = random.randint(1, 99999999)
+    zakazId = random.randint(1, 99999999)
+    zakazSum = int(input('Введите кол-во товара:'))
+    
+    def unit(self):
+    #Оформление Заказа на площадке 
+      self.zakazNumbers = random.randint(1, 99999999)
+      self.zakazId = random.randint(1, 99999999)
+      self.zakazSum = int(input('Введите кол-во товара:'))
+    if (((self.zakazSum) < 1000000) & ((self.zakazSum) > 0)):
+        print("Заказ оформлен")
+    else:
+        print('Недопустимое количество')
 
 
-storageNumber = range(1, 10)
-storageSum = int(999999999)
+class Sklad:
+     storageNumber =  range(1, 10)
+     def new_func():
+     storageSum = int(999999999)
+    
+    def unit(self):
+    #Проверка товара на складе 
+      self.storageNumber = range(1, 10)
+      self.storageSum = int(999999999)
 
-productId = random.randint(1, 99999999)
-productName = str(input('Введите название товара:'))
-if len(productName) < 100:
+
+class Tovar:
+    productId = random.randint(1, 99999999)
+    productName = str(input('Введите название товара:'))
+    
+def unit(self):    
+# Проверка товара на маркетплейсе и его добавление 
+  self.productId = random.randint(1, 99999999)
+  self.productName = str(input('Введите название товара:'))
+if len(self.productName) < 100:
     print('Товар в наличии')
 else:
     print('Такого товара нет')
